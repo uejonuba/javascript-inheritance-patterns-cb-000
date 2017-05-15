574b96f931ad4db5352a8a7cda9215f22e8e7959
Uzor Jon-Ubabuco <uubabukoh@gmail.com>
2017-05-15 20:11:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+function Software(size, name, type){
+  this.size = size;
+  this.name = name;
+  this.type = "Applicaiton";
+}
+
+function Game(size,name,genre){
+  Software.call(size,name,"Game")
+
+  this.genre = genre;
+}
+
+Game.prototype = Object.create(Software.prototype);
+Game.prototype.constructor = Game;
+
+Game.prototype.players = "1 Player";
+Game.prototype.description = function functionName() {
+  return `Game: ${this.name} \n Genre: ${this.genre}`
+}
+
+
+var gameMario = new Game(4,"Super Mario RPG: The Legend of the Seven Stars", "RPG")
